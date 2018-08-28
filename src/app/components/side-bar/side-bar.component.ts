f9d7f47d535c6becbeea58733f3a15ae98e47720
ysmolyar <yonismolyar1@gmail.com>
2018-08-28 18:27:48 -0400

    created responsive collapsible side-bar

---
+++ src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,63 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
+
+@Component({
+  selector: 'app-side-bar',
+  templateUrl: './side-bar.component.html',
+  styleUrls: ['./side-bar.component.css']
+})
+export class SideBarComponent implements OnInit {
+  isActive = false;
+  collapsed = false;
+  showMenu = '';
+  pushRightClass = 'push-right';
+
+  @Output() collapsedEvent = new EventEmitter<boolean>();
+
+  constructor(public router: Router) {
+
+    this.router.events.subscribe(val => {
+      if (
+        val instanceof NavigationEnd &&
+        window.innerWidth <= 992 &&
+        this.isToggled()
+      ) {
+        this.toggleSidebar();
+      }
+    });
+  }
+
+  ngOnInit() {
+  }
+
+  eventCalled() {
+    this.isActive = !this.isActive;
+  }
+
+  addExpandClass(element: any) {
+    if (element === this.showMenu) {
+      this.showMenu = '0';
+    } else {
+      this.showMenu = element;
+    }
+  }
+
+  toggleCollapsed() {
+    this.collapsed = !this.collapsed;
+    this.collapsedEvent.emit(this.collapsed);
+  }
+
+  isToggled(): boolean {
+    const dom: Element = document.querySelector('body');
+    return dom.classList.contains(this.pushRightClass);
+  }
+
+  toggleSidebar() {
+    const dom: any = document.querySelector('body');
+    dom.classList.toggle(this.pushRightClass);
+  }
+
+  onLoggedout() {
+    localStorage.removeItem('isLoggedin');
+  }
+}
